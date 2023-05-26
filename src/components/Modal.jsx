561e847346a531ed663130e74df7ea16167cb38e
kimballcaroline <kimballcaroline@gmail.com>
2023-05-26 15:24:56 -0400

    replace heart emoji with custom heart icon

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,11 @@
+import HeartIcon from '../assets/icons/icon_heart.png';
 import '../stylesheets/Modal.scss';
 
 export const Modal = () => {
   return (
     <div className="modal">
       <h1>Welcome to Pixel Pets!</h1>
-      <p>Use the buttons on the bottom to select the controls, feed, clean, or give pets &#10084;</p>
+      <p>Use the buttons on the bottom to select the controls, feed, clean, or give pets <img src={HeartIcon} alt="heart icon" /></p>
       <p>To start the game, press the center button!</p>
     </div>
   )
