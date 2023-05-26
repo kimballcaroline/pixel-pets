45830f20e2f2e78f55c4cfcede497b2a7fb557e9
kimballcaroline <kimballcaroline@gmail.com>
2023-05-26 14:15:12 -0400

    add modal component

---
+++ src/components/Modal.jsx
@@ -0,0 +1,11 @@
+import '../stylesheets/Modal.scss';
+
+export const Modal = () => {
+  return (
+    <div className="modal">
+      <h1>Welcome to Pixel Pets!</h1>
+      <p>Use the buttons on the bottom to select the controls, feed, clean, or give pets &#10084;</p>
+      <p>To start the game, press the center button!</p>
+    </div>
+  )
+}
